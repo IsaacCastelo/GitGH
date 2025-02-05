ec17c048e20a307fe17320094093b4fa7beb8235
IsaacCastelo <isaacpxl8@gmail.com>
2025-02-04 20:55:53 -0700

    adding api key

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Platzi!");
+string = stripe_api_key = "Q29uZ3JhdHVsYXRpb25zIVRoaXMgaXMgYSByYW5kb20gYXBpIGtleSE=";
